430d07b410473090f04187d4c5f1d59aad2d8033
Rylan Stevens <rylanstevens96@gmail.com>
2025-03-04 20:21:27 -0600

    PDT website launch

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("infoBtn").addEventListener("click", function() {
+    let info = document.getElementById("info");
+    info.style.display = info.style.display === "none" ? "block" : "none";
+});
